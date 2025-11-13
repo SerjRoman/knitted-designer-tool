86946907807230bf60d712cb4ea7270148b0d86e
SerjRoman <sergeyroman26@gmail.com>
2025-11-13 09:57:54 +0000

    feat: refactor ChangeGridSizesModal and ChangeGridSizes components for improved state management and modal handling fix: update import path for RGBColor type in calculateColorDistance function fix: correct export path for createAppAsyncThunk in index.ts

--- src/features/change-grid-sizes/ui/change-grid-sizes/change-grid-sizes.tsx
+++ src/features/change-grid-sizes/ui/change-grid-sizes/change-grid-sizes.tsx
@@ -2,20 +2,24 @@ import { useAppSelector, useModal } from "@/shared/lib";
 import { ChangeGridSizesModal } from "../change-grid-sizes-modal";
 
 export function ChangeGridSizes() {
-	const [{ open }, ModalProvider] = useModal();
+	const [{ open, close }, ModalProvider] = useModal();
 	const { numberColumns, numberRows } = useAppSelector(
 		(state) => state.canvas
 	);
 	return (
-		<button
-			className=" w-full bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-xl p-4 text-center shadow-lg transition-all duration-200"
-			onClick={() => open()}
-		>
-			<div className="text-sm opacity-90">Grid Size / Resize</div>
-			<div className="text-3xl font-bold mt-1 tracking-wide">
-				{numberColumns} × {numberRows}
-			</div>
-			<ModalProvider ModalComponent={ChangeGridSizesModal} />
-		</button>
+		<>
+			<button
+				className=" w-full bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-xl p-4 text-center shadow-lg transition-all duration-200"
+				onClick={() => open()}
+			>
+				<div className="text-sm opacity-90">Grid Size / Resize</div>
+				<div className="text-3xl font-bold mt-1 tracking-wide">
+					{numberColumns} × {numberRows}
+				</div>
+			</button>
+			<ModalProvider>
+				<ChangeGridSizesModal onClose={close} />
+			</ModalProvider>
+		</>
 	);
 }
--- src/features/select-color/ui/select-color/select-color.tsx
+++ src/features/select-color/ui/select-color/select-color.tsx
@@ -1,5 +1,11 @@
+import { PlusIcon } from "lucide-react";
 import { setCurrentColor } from "@/entities/editor";
-import { useAppDispatch, useAppSelector, useModal } from "@/shared/lib";
+import {
+	MAX_COLORS,
+	useAppDispatch,
+	useAppSelector,
+	useModal,
+} from "@/shared/lib";
 import { SelectCustomColorModal } from "../select-custom-color-modal";
 
 export function SelectColor() {
@@ -9,15 +15,25 @@ export function SelectColor() {
 	const [{ open: openModal }, ModalProvider] = useModal<{
 		selectedColor: string;
 	}>();
-
+	const maxColorsExceeded = MAX_COLORS <= colors.length;
 	return (
-		<div className="flex flex-row space-2 gap-3">
-			<button
-				onClick={() => openModal({ selectedColor: currentColor })}
-				className="w-[15%] flex items-center justify-center bg-white border border-gray-300 rounded-lg text-gray-500 hover:bg-gray-50 transition"
-				style={{ backgroundColor: currentColor }}
-			></button>
-			<div className="grid grid-cols-5 gap-1">
+		<div className="grid grid-cols-7 gap-3 ">
+			<div className="col-span-2 grid grid-rows-2 gap-3">
+				<button
+					onClick={() => openModal({ selectedColor: currentColor })}
+					className="row-span-1 flex items-center justify-center border border-gray-300 rounded-lg transition hover:brightness-90"
+					style={{ backgroundColor: currentColor }}
+				></button>
+				<button
+					className={`flex flex-row items-center justify-center p-2 rounded-lg border-2 transition-all duration-200 ${
+						maxColorsExceeded && "opacity-50 cursor-not-allowed"
+					}`}
+					disabled={maxColorsExceeded}
+				>
+					<PlusIcon size={20} /> Add
+				</button>
+			</div>
+			<div className="col-span-5 grid grid-cols-4 gap-1">
 				{colors.map((color, index) => (
 					<button
 						key={index}
